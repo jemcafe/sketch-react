0f57b62d64a92546f5a24d7940cf99d0f6b63946
Jemaine Brown <jembrown@jemaines-mbp.attlocal.net>
2018-06-15 01:11:50 -0500

    Setup sass. Created canvas component and container. Canvas can be engaged.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,21 +3,34 @@ import PropTypes from 'prop-types';
 
 class Canvas extends Component {
    
-   componentDidMount () {
-      this.props.initCanvas(this.refs.canvas);
-   }
+  componentDidMount () {
+    this.props.initCanvas(this.refs.canvas);
+  }
 
-   render() {
-      return (
-         <div className="canvas">
-            <canvas ref="canvas"/>
-         </div>
-      );
-   }
+  render() {
+    const { focus, engage, disengage } = this.props;
+
+    return (
+      <div className="canvas">
+
+        { focus && 
+        <div className="focus-overlay" 
+          onMouseUp={() => disengage(this.refs.canvas)}
+          onMouseLeave={() => disengage(this.refs.canvas)}>
+        </div> }
+
+        <canvas ref="canvas" onMouseDown={(e) => engage(this.refs.canvas, e)}/>
+
+      </div>
+    );
+  }
 }
 
 Canvas.propTypes = {
-   initCanvas: PropTypes.func.isRequired
+  focus: PropTypes.bool.isRequired,
+  initCanvas: PropTypes.func.isRequired,
+  engage: PropTypes.func.isRequired,
+  disengage: PropTypes.func.isRequired
 };
 
 export default Canvas;
--- src/containers/CanvasCntr.js
+++ src/containers/CanvasCntr.js
@@ -2,44 +2,60 @@ import React, { Component } from 'react';
 import Canvas from '../components/Canvas';
 
 class CanvasCntr extends Component {
-   constructor () {
-      super();
-      this.state = {
-         mouse: { x: 0, y: 0 },
-         dragging: false,
-         focus: false
-      }
-   }
+  constructor () {
+    super();
+    this.state = {
+      mouse: {
+        x: 0,
+        y: 0 
+      },
+      brush: {
+        radius: 5,
+        color: '#000',
+        selected: true
+      },
+      eraser: {
+        radius: 5,
+        color: '#fff',
+        selected: false
+      },
+      dragging: false,
+      focus: false
+    }
+  }
 
-   initCanvas = (canvas) => {
-      // Canvas size
-      canvas.width = 400;
-      canvas.height = 400;
+  initCanvas = (canvas) => {
+    // Canvas size
+    canvas.width = 400;
+    canvas.height = 400;
 
-      // Context of the canvas
-      const context = canvas.getContext('2d');
+    // Context of the canvas
+    const context = canvas.getContext('2d');
 
-      // The intial color of the canvas
-      context.fillStyle = '#f0f0f0';
-      context.fillRect(0, 0, canvas.width, canvas.height);
-   }
+    // The intial color of the canvas
+    context.fillStyle = '#f0f0f0';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
 
-   engage = (canvas, e) => {
-      this.setState({ dragging: true, focus: true });
-   }
+  engage = (canvas, e) => {
+    console.log('engaged');
+    this.setState({ dragging: true, focus: true });
+  }
 
-   disengage = (canvas) => {
-      this.setState({ dragging: false, focus: false });
-   }
+  disengage = (canvas) => {
+    console.log('disengaged');
+    this.setState({ dragging: false, focus: false });
+  }
 
-   render() {
-      return (
-         <Canvas
-            initCanvas={ this.initCanvas }
-            engage={ this.engage }
-            disengage={ this.disengage } />
-      );
-   }
+  render() {
+    return (
+      <Canvas
+        focus={ this.state.focus }
+        initCanvas={ this.initCanvas }
+        engage={ this.engage }
+        disengage={ this.disengage } />
+    );
+  }
 }
 
 export default CanvasCntr;
